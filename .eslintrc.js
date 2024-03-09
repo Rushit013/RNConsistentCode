5b0b383acc3f2f2c9d8c0a5e8526d7b549c42969
Rushit <rushitjivani1999@gmail.comrushitjivani1999@gmail.comrushitjivani1999@gmail.com>
2024-03-09 11:27:52 +0530

    Configure "ESLint" and "Prettier"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-use-before-define': [
+      'error',
+      { functions: true, classes: true, variables: false },
+    ],
+    'react-native/no-unused-styles': 0,
+    'react-native/split-platform-components': 0,
+    'react-native/no-color-literals': 0,
+    'react-native/no-raw-text': 0,
+    'react-native/no-single-element-style-arrays': 0,
+    'react-native/no-inline-styles': 0,
+    'no-extra-semi': 'off',
+  },
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: 'all',
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: false,
 };
